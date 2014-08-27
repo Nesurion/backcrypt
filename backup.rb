c406896f57412fe4165cd7fd7c383b94912a942f
Kai <kai@kaimac.fritz.box>
2014-08-27 19:27:00 +0200

    Options
    Overwriting options, language and compression options added

--- backup.rb
+++ backup.rb
@@ -7,8 +7,17 @@ directory = '/Users/kai/Desktop/test/'
 # Destination path
 zipfile_name = '/Users/kai/Desktop/archive.zip'
 
-Zip.on_exists_proc = true
-Zip.continue_on_exists_proc = true
+# Archive configuration
+Zip.setup do |c|
+    # Overwrite files
+    c.on_exists_proc = true
+    # Overwrite existing .zip files
+    c.continue_on_exists_proc = true
+    # Abbility to store archives with non-english names
+    c.unicode_names = true
+    # Compression level: Best possible options: DEFAULT, BEST, NO
+    c.default_compression = Zlib::BEST_COMPRESSION
+  end
 
 Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
     Dir[File.join(directory, '**', '**')].each do |file|
